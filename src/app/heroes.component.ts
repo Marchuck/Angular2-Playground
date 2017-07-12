e86d30fe6e983ef72101aa1aa4505344a0d317ee
Łukasz <lukasz@next42.net>
2017-07-12 13:54:15 +0200

    Angular2 start

---
+++ src/app/heroes.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Router }   from '@angular/router';
+
+//import './rxjs-operators';
+import {Hero} from './hero';
+import { HeroService } from './hero.service';
+
+@Component({
+  selector: 'my-heroes',
+  templateUrl: './heroes.component.html',
+  providers: [HeroService],
+  styleUrls: ['./app.component.css']
+})
+
+
+
+export class HeroesComponent implements OnInit{
+    
+  constructor(private heroService: HeroService,
+               private router: Router) { }
+  
+  title = 'Tour of Heroes';
+  heroes: Hero[] = [];
+  selectedHero: Hero;
+  onSelect(hero: Hero) : void {
+        this.selectedHero = hero;
+  }
+  getHeroes(): void {
+      this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  
+  ngOnInit(): void {
+     this.getHeroes();
+  }
+  gotoDetail(): void {
+      this.router.navigate(['/detail', this.selectedHero.id]);
+  }
+}
